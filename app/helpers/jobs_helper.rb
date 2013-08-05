f504777355c49aedfbcca0c8ec2b5a6901279ea7
John Kueh <yoongfook@gmail.com>
2013-08-06 09:45:35 +1000

    removed job numbers from homepage

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,4 +2,12 @@ module JobsHelper
 	def total_search_result
 		return @search_results.length
 	end
+
+	def total_jobs
+		return Job.all.length
+	end
+
+	def number_of_jobs_in_category(category)
+		category.subcategories.map { |s| s.jobs.length }.inject(:+)
+	end
 end
