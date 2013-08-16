856fb312d619614697d63fff833efb2a5d675238
John Kueh <yoongfook@gmail.com>
2013-08-16 11:36:09 +1000

    [finishes #55206820]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,23 @@ class ApplicationController < ActionController::Base
 	  redirect_to root_url, :alert => exception.message
 	end
 
+	after_filter :store_location
+
+	def store_location
+	 # store last url - this is needed for post-login redirect to whatever the user last visited.
+	    if (request.fullpath != "/users/sign_in" && \
+	        request.fullpath != "/users/sign_up" && \
+	        request.fullpath != "/users/password" && \
+	        !request.xhr?) # don't store ajax calls
+	      session[:previous_url] = request.fullpath
+	    end
+	end
+
 	def after_sign_in_path_for(resource)
 		if current_user.has_role? :admin
 			admin_jobs_path
 		else
-			root_path
+			session[:previous_url] || root_path
 		end
 	end
 
