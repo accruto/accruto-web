f69c1a654c807325e2c565ef837eed931a173abc
John Kueh <yoongfook@gmail.com>
2013-09-16 17:53:49 +1000

    added how it works section to homepage

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -33,7 +33,7 @@ RailsAdmin.config do |config|
   config.included_models = ['Address', 'CandidateSearchBetaUser', 'Company', 'Favourite', 'Job', 'JobApplication', 'JobCategory', 'JobSubcategory', 'ReferralSite', 'User']
 
   # Label methods for model instances:
-  # config.label_methods << :description # Default is [:name, :title]
+  config.label_methods = [:name, :title, :city]# Default is [:name, :title]
 
 
   ################  Model configuration  ################
