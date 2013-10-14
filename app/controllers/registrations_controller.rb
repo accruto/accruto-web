29a4f2926271f26735211ee9dee856b5636bbc2d
John Kueh <yoongfook@gmail.com>
2013-10-14 11:24:02 +1100

    fixed invite bug on registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,7 +10,9 @@ class RegistrationsController < Devise::RegistrationsController
     build_resource(params[:user])
     if resource.save
       @invite = get_invite_details
-      @invite.signed_up!
+      if @invite
+        @invite.signed_up!
+      end
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, resource)
